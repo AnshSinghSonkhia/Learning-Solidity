5e401296e21e069a704655e78b0d121411d12707
Ansh Singh Sonkhia <110414565+AnshSinghSonkhia@users.noreply.github.com>
2023-04-20 22:50:32 +0530

    Create Counter.sol

---
+++ Counter.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;     // That's the minimum version needed for the smart contract, that we're developing.
+
+contract Counter {          // File name is used after `contract` ---> Here, i.e., `Counter`
+    // Code goes here...    
+
+    count;                  // State Variables          Video at 4:00
+}
