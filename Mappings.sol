7e27b7e557f89f37ef1f13552d1e471e29b2e175
Ansh Singh Sonkhia <110414565+AnshSinghSonkhia@users.noreply.github.com>
2023-04-22 22:36:40 +0530

    Update Mappings.sol

--- Mappings.sol
+++ Mappings.sol
@@ -3,4 +3,39 @@ pragma solidity ^0.8.0;
 
 contract Mappings {
 
+    // -----------------MAPPINGS-----------------
+
+    // mapping(key => value) myMappings;
+    
+    mapping(uint256 => string) public names;
+    mapping(uint256 => Book) public books;
+    mapping(address => mapping(uint256 => Book)) public myBooks;
+
+
+    struct Book {
+        string title;
+        string author;
+    }
+
+    constructor() {
+        names[1] = "Aditya";
+        names[2] = "Bruicy";
+        names[3] = "Chapati Bhai";
+    }
+
+    function addBook(
+        uint256 _id,
+        string memory _title,
+        string memory _author
+    ) public {
+        books[_id] = Book(_title, _author);
+    }
+
+    function addMyBook(
+        uint256 _id,
+        string memory _title,
+        string memory _author
+    ) public {
+        myBooks[msg.sender][_id] = Book(_title, _author);
+    }
 }
