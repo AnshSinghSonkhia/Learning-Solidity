afab3f7c50af8b0f766645a1ba91e14ec7b57016
Ansh Singh Sonkhia <110414565+AnshSinghSonkhia@users.noreply.github.com>
2023-04-22 22:18:58 +0530

    Update Arrays_inDetail.sol

--- Arrays_inDetail.sol
+++ Arrays_inDetail.sol
@@ -6,5 +6,21 @@ contract Arrays_inDetail {
     // -----------------ARRAYS-----------------
 
     uint[] public uintArray = [1,2,3];
+    string[] public stringArray = ["apple", "banana", "carrot"];
+    string[] public values;
+
+    // -----------------2D ARRAYS-----------------
+
+    uint256[][] public array2D = [[1, 2, 3], [4, 5, 6]];
+
+    // -----------------Modify The Arrays-----------------    
+
+    function addValue(string memory _value) public {
+        values.push(_value);
+    }
+
+    function valueCount() public view returns (uint256) {
+    return values.length;
+    }
 
 }
