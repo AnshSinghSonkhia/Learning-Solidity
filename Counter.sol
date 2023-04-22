0257a9144b6540c312040856ac4386a8129566ef
Ansh Singh Sonkhia <110414565+AnshSinghSonkhia@users.noreply.github.com>
2023-04-22 11:17:02 +0530

    Update Counter.sol

--- Counter.sol
+++ Counter.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;     // That's the minimum version needed for the smart c
 contract Counter {          // File name is used after `contract` ---> Here, i.e., `Counter`
     // Code goes here...    
 
-    uint count;                  // State Variables         
+    uint public count = 0;                  // State Variables         // using public here gives us the count function for free. Now we don't need getCount()
                                 /* 
                                 Unsigned Integer:
                                 declared with the uint keyword, is a value data type that must be non-negative;
@@ -12,13 +12,17 @@ contract Counter {          // File name is used after `contract` ---> Here, i.e
                                 Just like signed integers, this value data type takes up to 32B by default. 
                                 */
 
+/*
     constructor() public {
         count = 0;
     }
+*/
 
+/*
     function getCount() public view returns(uint) {
         return count;
     }
+*/
 
      function incrementCount() public {
         // count = count + 1;
