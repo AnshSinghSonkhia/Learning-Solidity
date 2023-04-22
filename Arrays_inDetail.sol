eec3787b10564c6d2a49e8230bd9641084bb5f68
Ansh Singh Sonkhia <110414565+AnshSinghSonkhia@users.noreply.github.com>
2023-04-22 20:22:18 +0530

    Create Arrays_inDetail.sol

---
+++ Arrays_inDetail.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Arrays_inDetail {
+
+    // -----------------ARRAYS-----------------
+
+    uint[] public uintArray = [1,2,3];
+
+}
